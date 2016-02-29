a7cae4321f829311235c975b5d426dd20b7c7aea
alu0100614220 <alu0100614220@ull.edu.es>
2016-02-29 23:05:32 +0000

    empezando, añadida conversion entre f y c

--- assets/js/conversor.js
+++ assets/js/conversor.js
@@ -1,59 +1,69 @@
 (function(exports) {
-  "use strict";
-
-  function Medida(valor,tipo)  
-  {
-    /* tipo es opcional. Debería admitir  new Medida("45.2 Km") */
-    /* ademas de new Medida(45.2, "Km") */
-  }
-  
-  function Temperatura(valor,tipo)
-  {
-    /* tipo es opcional. Debería admitir new Medida("45.2 F") */
-  }
-  
-  function Celsius(valor)
-  {
-  }
-  
-  function Farenheit(valor)
-  {
-  }
-  
-  exports.Temperatura = Temperatura;
-  exports.Celsius = Celsius;
-  exports.Farenheit = Farenheit;
-
-  exports.convertir = function() {
-    var valor     = document.getElementById('convert').value,
-        elemento  = document.getElementById('converted'),
-        /* Extienda la RegeExp a la especificación. use una XRegExp */
-        regexp    = /^\s*([-+]?\d+(?:\.\d+)?(?:e[+-]?\d+)?)\s*([a-z,A-Z]+)\s*$/i,
-        valor     = valor.match(regexp);
-    
-    if (valor) {
-      var numero = valor[1],
-          tipo   = valor[2].toLowerCase();
-      
-      numero = parseFloat(numero);
-      console.log("Valor: " + numero + ", Tipo: " + tipo);
-      
-      switch (tipo) {
-        case 'c':
-          var celsius = new Celsius(numero);
-          elemento.innerHTML = celsius.toFarenheit().toFixed(2) + " Farenheit";
-          break;
-        case 'f':
-          var farenheit = new Farenheit(numero);
-          elemento.innerHTML = farenheit.toCelsius().toFixed(2) + " Celsius";
-          break;
-        
-        default:
-          /* rellene este código */
-      }
+    "use strict";
+    function selectMedida(medida){
+      console.log(medida);
     }
-    else
-      elemento.innerHTML = "";
-  }
-  
+    function Medida(valor, tipo) {
+        /* tipo es opcional. Debería admitir  new Medida("45.2 Km") */
+        /* ademas de new Medida(45.2, "Km") */
+    }
+
+    function Temperatura(valor, tipo) {
+        /* tipo es opcional. Debería admitir new Medida("45.2 F") */
+    }
+
+    function Celsius(valor) {
+        this.toFarenheit = function() {
+            valor = (valor * 9 / 5) + 32;
+            return valor;
+        }
+    }
+
+    function Farenheit(valor) {
+        this.toCelsius = function() {
+            valor = (valor - 32) * 5 / 9;
+            return valor;
+        }
+    }
+
+    exports.Temperatura = Temperatura;
+    exports.Celsius = Celsius;
+    exports.Farenheit = Farenheit;
+
+    exports.convertir = function() {
+        var valor = document.getElementById('convert').value,
+            elemento = document.getElementById('converted'),
+            /* Extienda la RegeExp a la especificación. use una XRegExp */
+            regexp = /^\s*([-+]?\d+(?:\.\d+)?(?:e[+-]?\d+)?)\s*([a-z,A-Z]+)\s*$/i,
+            valor = valor.match(regexp);
+
+        if (valor) {
+            var numero = valor[1],
+                tipo = valor[2].toLowerCase();
+
+            numero = parseFloat(numero);
+            console.log("Valor: " + numero + ", Tipo: " + tipo);
+
+            switch (tipo) {
+                case 'c':
+                    var celsius = new Celsius(numero);
+                    elemento.innerHTML = celsius.toFarenheit().toFixed(2) + " Farenheit";
+                    break;
+                case 'f':
+                    var farenheit = new Farenheit(numero);
+                    elemento.innerHTML = farenheit.toCelsius().toFixed(2) + " Celsius";
+                    break;
+
+                default:
+                    /* rellene este código */
+            }
+        } else
+            elemento.innerHTML = "";
+    }
+
+    exports.selectMedida = function(){
+      var valor = document.getElementById('medida').value;
+      console.log(valor)
+    }
+
 })(this);
